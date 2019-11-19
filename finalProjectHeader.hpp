375850732ff6d4de56499f1ad61fe70e174f3af5
bouldercoder9 <51352526+bouldercoder9@users.noreply.github.com>
2019-11-19 10:34:52 -0700

    Init commit for this dev branch

---
+++ finalProjectHeader.hpp
@@ -0,0 +1,36 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+#include<vector>
+#include<iostream>
+using namespace std;
+
+struct node;
+struct adjNode{
+  node *n;
+};
+
+struct node{
+  node(char newChar, node *par){
+    this->searchCount = 0;
+    this->c = newChar;
+    this->parent = par;
+    this->terminal = false;
+    this->adj[0] = NULL;
+  }
+  char c;
+  vector<adjChar> adj;
+  node* parent;
+  int searchCount;
+  bool terminal;
+};
+
+class trie{
+  public:
+    void insert();
+    void deleter();
+    string bestGuess(string input);
+  private:
+    node *root;
+};
+
+#endif
